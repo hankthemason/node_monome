dbc419fecd90efc4ffa7e6223bae59cb8aa0fd51
hank <hankthemason@gmail.com>
2021-08-10 12:59:51 -0400

    included scales object for main 7 note scales

---
+++ configurations/scales.js
@@ -0,0 +1,20 @@
+const scales = [
+	{
+		scaleName: 'major',
+		scale: [0, 2, 4, 5, 7, 9, 11]
+	},
+	{
+		scaleName: 'minor',
+		scale: [0, 2, 3, 5, 7, 8, 10]
+	},
+	{
+		scaleName: 'dorian',
+		scale: [0, 2, 3, 5, 7, 9, 10]
+	},
+	{
+		scaleName: 'mixolydian',
+		scale: [0, 2, 4, 5, 7, 9, 10]
+	}
+]
+
+module.exports = scales
