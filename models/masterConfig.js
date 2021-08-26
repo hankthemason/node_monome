0126c66ad6c86526eec95d24cb2c86a791cd8c0d
hankthemason <42900752+hankthemason@users.noreply.github.com>
2021-08-26 11:49:31 -0400

    added swing attribute to masterConfig model

--- models/masterConfig.js
+++ models/masterConfig.js
@@ -1,14 +1,15 @@
 class MasterConfig {
-  constructor(tracks, masterTrack, masterHz, syncing, syncTrack, noteValues, currentTrack, copying, universalNoteValueOn) {
-    this.tracks = tracks,
-      this.masterTrack = masterTrack,
-      this.masterHz = masterHz,
-      this.syncing = syncing,
-      this.syncTrack = syncTrack,
-      this.noteValues = noteValues,
-      this.currentTrack = currentTrack,
-      this.copying = copying,
-      this.universalNoteValueOn = universalNoteValueOn
+  constructor(tracks, masterTrack, masterHz, syncing, syncTrack, noteValues, currentTrack, copying, universalNoteValueOn, swing) {
+    this.tracks = tracks;
+    this.masterTrack = masterTrack;
+    this.masterHz = masterHz;
+    this.syncing = syncing;
+    this.syncTrack = syncTrack;
+    this.noteValues = noteValues;
+    this.currentTrack = currentTrack;
+    this.copying = copying;
+    this.universalNoteValueOn = universalNoteValueOn;
+    this.swing = swing;
   }
 
   updateCurrentTrack(newCurrentTrack) {
